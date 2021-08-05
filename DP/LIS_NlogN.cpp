fd44930734ac750b18dcee94a07b58be0e1a2086
Junhyuk Han <jhhan128@gmail.com>
2021-08-05 20:07:41 +0900

    Update LIS_NlogN.cpp

--- DP/LIS_NlogN.cpp
+++ DP/LIS_NlogN.cpp
@@ -1,11 +1,3 @@
-/*
- * Longest Increasing Subsequence in O(NlogN)
- * 
- * n is length of an array
- * after all work done, size of vector v is length of LIS
- *
- */
-
 int n; cin >> n;
 int arr[n];
 for (int i = 0; i < n; i++) cin >> arr[i];
@@ -16,4 +8,4 @@ for (int i = 0; i < n; i++) {
     else *lower_bound(v.begin(), v.end(), arr[i]) = arr[i];
 }
 
-cout << v.size();
+// v.size() is length of LIS
