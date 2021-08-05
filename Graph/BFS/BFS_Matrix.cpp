299eceb73722e9e4bac0c63496250493c71c61d8
Junhyuk Han <jhhan128@gmail.com>
2021-08-05 22:10:31 +0900

    Update BFS_Matrix.cpp

--- Graph/BFS/BFS_Matrix.cpp
+++ Graph/BFS/BFS_Matrix.cpp
@@ -1,18 +1,18 @@
-int maps[1001][1001];
+int adj[1001][1001];
 bool visited[1001];
 int n;
 
 void bfs(int v) {
     queue<int> q;
     q.push(v);
-    visited2[v] = 1;
+    visited[v] = 1;
 
     while (!q.empty()) {
         v = q.front();
         q.pop();
 
         for (int i = 1; i <= n; i++) {
-            if (maps[v][i] && !visited[i]) {
+            if (adj[v][i] && !visited[i]) {
                 visited[i] = 1;
                 q.push(i);
             }
