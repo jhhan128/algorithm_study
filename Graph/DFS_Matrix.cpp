0389cb3314fefd32f22978ab2193242b4096cb17
Junhyuk Han <jhhan128@gmail.com>
2021-08-05 18:49:57 +0900

    Create DFS_Matrix.cpp

---
+++ Graph/DFS_Matrix.cpp
@@ -0,0 +1,10 @@
+int maps[1001][1001];
+int visited[1001];
+int n;
+
+void dfs(int v) {
+    visited[v] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (maps[v][i] && !visited[i]) dfs(i);
+    }
+}
