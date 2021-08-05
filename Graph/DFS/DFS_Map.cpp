56bb77c1f5783e327e46bc24b7ff5cd04045137e
Junhyuk Han <jhhan128@gmail.com>
2021-08-05 20:01:55 +0900

    Create DFS_Map.cpp

---
+++ Graph/DFS/DFS_Map.cpp
@@ -0,0 +1,19 @@
+int board[101][101];
+bool visited[101][101];
+int w, h;
+
+int dx[] = { 0, -1, 1, 0 };
+int dy[] = { -1, 0, 0, 1 };
+
+void dfs(int x, int y) {
+    visited[x][y] = 1;
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+
+        if (nx < 0 || ny < 0 || nx >= w || ny >= h) continue;
+        if (!board[nx][ny] || visited[nx][ny]) continue;
+
+        dfs(nx, ny);
+    }
+}
