7137d5d89cae904e64297bf9541defc5245c68cb
Junhyuk Han <jhhan128@gmail.com>
2021-08-05 22:09:46 +0900

    Update DFS_Matrix.cpp

--- Graph/DFS/DFS_Matrix.cpp
+++ Graph/DFS/DFS_Matrix.cpp
@@ -1,10 +1,10 @@
-int maps[1001][1001];
+int adj[1001][1001];
 bool visited[1001];
 int n;
 
 void dfs(int v) {
     visited[v] = 1;
     for (int i = 1; i <= n; i++) {
-        if (maps[v][i] && !visited[i]) dfs(i);
+        if (adj[v][i] && !visited[i]) dfs(i);
     }
 }
