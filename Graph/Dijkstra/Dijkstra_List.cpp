11795393e9871aa2a29f0cef7ee03b7b31e06ac6
Junhyuk Han <jhhan128@gmail.com>
2021-08-07 20:03:50 +0900

    Create Dijkstra_List.cpp

---
+++ Graph/Dijkstra/Dijkstra_List.cpp
@@ -0,0 +1,31 @@
+const int MAX = 20001;
+const int INF = 0x3f3f3f3f;
+
+vector<pair<int, int>> adj[MAX];
+int v, e, k, dist[MAX];
+
+void dijkstra(int v) {
+    using pii = pair<int, int>;
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    memset(dist, INF, sizeof(dist));
+
+    dist[v] = 0;
+    pq.push({ 0, v });
+
+    while (!pq.empty()) {
+        int c = pq.top().first;
+        int n = pq.top().second;
+        pq.pop();
+
+        if (dist[n] < c) continue;
+        for (int i = 0; i < adj[n].size(); i++) {
+            int nn = adj[n][i].first;
+            int nc = c + adj[n][i].second;
+
+            if (dist[nn] > nc) {
+                dist[nn] = nc;
+                pq.push({ nc, nn });
+            }
+        }
+    }
+}
