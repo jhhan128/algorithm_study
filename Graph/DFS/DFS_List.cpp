f7b9efe31674b65a8721097e9c1ad8bc46939b3a
Junhyuk Han <jhhan128@gmail.com>
2021-09-12 19:10:19 +0900

    Update DFS_List.cpp

--- Graph/DFS/DFS_List.cpp
+++ Graph/DFS/DFS_List.cpp
@@ -7,7 +7,6 @@ void dfs(int v) {
     visited[v] = 1;
     for (int i = 0; i < adj[v].size(); i++) {
         int nxt = adj[v][i];
-        if (visited[nxt]) continue;
-        dfs(nxt);
+        if (!visited[nxt]) dfs(nxt);
     }
 }
