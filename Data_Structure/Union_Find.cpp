d78ab994965fa983225764a8f18c8ba20d5cb770
Junhyuk Han <jhhan128@gmail.com>
2021-09-24 12:04:58 +0900

    Create Union_Find.cpp

---
+++ Data_Structure/Union_Find.cpp
@@ -0,0 +1,29 @@
+struct UnionFind {
+    vector<int> par;
+
+    void init(int n) {
+        par.resize(n+3);
+
+        for (int i = 0; i <= n; i++) {
+            par[i] = -1;
+        }
+    }
+
+    int find(int v) {
+        if (par[v] < 0) return v;
+        return par[v] = find(par[v]);
+    }
+
+    void merge(int u, int v) {
+        u = find(u), v = find(v);
+        if (u == v) return;
+
+        if (par[v] > par[u]) swap(u, v);
+        par[u] += par[v];
+        par[v] = u;
+    }
+
+    int size(int v) {
+        return -par[find(v)];
+    }
+};
